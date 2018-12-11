c0fc631790e565f1f109254b18ad872709dcb160
Sylwia Rogala <sysa.rogala@gmail.com>
2018-12-11 19:15:35 +0100

    Zmiany w skrypcie pierwszy.py
    trututututu moge sie tu nizej rozpisywac. dodalismy informacje o tym, ze dzis jest 11 grudnia i sie ciężko uczymy

--- pierwszy.py
+++ pierwszy.py
@@ -3,3 +3,7 @@
 # to jest nasz pierwszy skrypt Pythona
 
 print ("Witaj swiecie! To jest nasz pierwszy skrypt pythona dzisiaj")
+
+#to drugi komentarz
+
+print ("Dziś jest 11 grudnia i uczymy się gita")
